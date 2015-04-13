3997bf68015f0d270652d343be912966adf51be8
qzshadow <qz1431880590@gmail.com>
2015-04-13 21:44:34 +0800

    first commit

---
+++ src/cn/scut/qinzhou/spider/model/SpiderParams.java
@@ -0,0 +1,6 @@
+package cn.scut.qinzhou.spider.model;
+
+public class SpiderParams {
+    public static int WORKER_NUM = 1;
+    public static int DEYLAY_TIME = 1000;
+}
---
+++ src/cn/scut/qinzhou/spider/storage/DataStorage.java
@@ -0,0 +1,8 @@
+package cn.scut.qinzhou.spider.storage;
+
+public class DataStorage {
+    public void store(Object data) {
+        // store to DB
+        // TODO
+    }
+}
